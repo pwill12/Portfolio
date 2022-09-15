bf9bc720051dc4828c48e8de57f1530d8693d3a5
unknown <princesolo919@gmail.com>
2022-09-15 23:10:10 +0100

    added preloader

--- src/components/pages/Pages.jsx
+++ src/components/pages/Pages.jsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
 import Footer from "../common/Footer"
 import { Header } from "../common/Header"
@@ -7,13 +7,25 @@ import { About } from "./About"
 import { Blog } from "./Blog"
 import { Contact } from "./Contact"
 import { Portfolio } from "./Portfolio"
+import Preloader from "./Preloader"
 import ResumeNew from "./Resume"
 import { Services } from "./Services"
 import { Testimonials } from "./Testimonials"
 export const Pages = () => {
+
+  const [load, upadateLoad] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      upadateLoad(false);
+    }, 1200);
+
+    return () => clearTimeout(timer);
+  }, []);
   return (
     <>
       <Router>
+        <Preloader load={load}/>
         <Header />
         <Switch>
           <Route exact path='/' component={Home} />
