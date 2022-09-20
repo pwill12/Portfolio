882c651d484577d051a07ba14a45780bd3b7de95
unknown <princesolo919@gmail.com>
2022-09-20 16:16:49 +0100

    updated

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -1,25 +1,26 @@
-import React from "react"
-import { Link } from "react-router-dom"
-import { Heading } from "../common/Heading"
-import { about } from "../data/dummydata"
+import React from "react";
+import { Link } from "react-router-dom";
+import { Heading } from "../common/Heading";
+import { about } from "../data/dummydata";
+import Links from "./Link";
 
 export const About = () => {
   return (
     <>
-      <section className='about'>
-        <div className='container flex'>
+      <section className="about">
+        <div className="container flex">
           {about.map((val) => (
             <>
-              <div className='left' data-aos='fade-down-right'>
-                <img src={val.cover} alt='' className="left-img"/>
+              <div className="left" data-aos="fade-down-right">
+                <img src={val.cover} alt="" className="left-img" />
               </div>
-              <div className='right' data-aos='fade-down-left'>
-                <Heading title='About Me' />
+              <div className="right" data-aos="fade-down-left">
+                <Heading title="About Me" />
                 <p>{val.desc}</p>
                 <p>{val.desc1}</p>
-                <button>Github Profile</button>
-                <Link to='/resume'>
-                <button className='primaryBtn'>Download CV</button>
+                <Links/>
+                <Link to="/resume">
+                  <button className="primaryBtn">Download CV</button>
                 </Link>
               </div>
             </>
@@ -27,5 +28,5 @@ export const About = () => {
         </div>
       </section>
     </>
-  )
-}
+  );
+};
---
+++ src/components/pages/Link.jsx
@@ -0,0 +1,8 @@
+import { Link } from "@mui/material";
+import React from "react";
+
+function Links() {
+  return <Link href="https://www.github.com/pwill12"><button>Github Profile</button></Link>;
+}
+
+export default Links;
